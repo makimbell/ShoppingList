11cb147a4e421b363b70bf0db2ee62930dca3420
Andy <andrew.kimbell@bargeacbl.com>
2020-11-24 16:33:02 -0500

    Beginning section 53

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Pie',
+      'Granny\'s chocolate pie.',
+      'https://i.pinimg.com/originals/9d/4f/0c/9d4f0c91e1a4af6c940f420e64fb4a91.jpg')
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
